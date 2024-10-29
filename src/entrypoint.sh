1720eb5f03ee822570a331531d156827f6d02900
Felipe Vieira <felipebcv@gmail.com>
2024-10-29 13:21:50 -0300

    Primeiro commit, ambiente configurado

---
+++ src/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ ! -d "/var/www/html/vendor" ]; then
+  composer install --prefer-dist --no-interaction
+  chown -R www-data:www-data /var/www/html
+  chmod -R 755 /var/www/html/vendor/*  
+fi
+
+until php artisan migrate --force; do
+  echo "Esperando pelo banco de dados..."
+  sleep 5
+done
